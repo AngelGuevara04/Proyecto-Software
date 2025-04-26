0ac264a06f0294feb490b57370ef4ecce67ba40f
Angel Guevara <130310134+AngelGuevara04@users.noreply.github.com>
2025-04-26 00:42:03 -0600

    Update script-login.js

--- script-login.js
+++ script-login.js
@@ -1,6 +1,6 @@
 // script-login.js
 import { auth, db } from './firebase-config.js';
-import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 import { doc, getDoc }               from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
 
 const loginForm = document.getElementById('login-form');
@@ -14,14 +14,11 @@ loginForm.addEventListener('submit', async e => {
     const { user } = await signInWithEmailAndPassword(auth, email, pass);
     const snap     = await getDoc(doc(db, "usuarios", user.uid));
     if (!snap.exists()) throw new Error("Usuario no encontrado.");
-
     const { rol } = snap.data();
-    // Redirigir según rol
-    if (rol === "alumno")    window.location.href = "dashboard-alumno.html";
-    else if (rol === "docente")  window.location.href = "dashboard-docente.html";
+    if (rol === "alumno")      window.location.href = "dashboard-alumno.html";
+    else if (rol === "docente")   window.location.href = "dashboard-docente.html";
     else if (rol === "ejecutivo") window.location.href = "dashboard-ejecutivo.html";
     else throw new Error("Rol no reconocido.");
-
   } catch (err) {
     console.error(err);
     alert("Error al iniciar sesión: " + err.message);
