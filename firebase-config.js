8596424581d89b0705d44bdea590199b3dd25eec
Angel Guevara <130310134+AngelGuevara04@users.noreply.github.com>
2025-04-25 23:55:03 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,21 @@
+// firebase-config.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCIU4fwzGijwNTkdieKW8Q60fMbn3rrwvg",
+  authDomain: "ingenova-6da56.firebaseapp.com",
+  databaseURL: "https://ingenova-6da56-default-rtdb.firebaseio.com",
+  projectId: "ingenova-6da56",
+  storageBucket: "ingenova-6da56.appspot.com",
+  messagingSenderId: "255346301463",
+  appId: "1:255346301463:web:35c7325c09ab634876da52",
+  measurementId: "G-H3FLH6S41Y"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
