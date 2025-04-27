c201605195de8d48880c77b1c0a722a1366266ff
Angel Guevara <130310134+AngelGuevara04@users.noreply.github.com>
2025-04-27 13:45:57 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 // firebase-config.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getAuth }        from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { initializeApp }  from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
 import { getFirestore }   from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
 import { getStorage }     from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js';
 
@@ -16,8 +15,7 @@ const firebaseConfig = {
 };
 
 const app     = initializeApp(firebaseConfig);
-const auth    = getAuth(app);
 const db      = getFirestore(app);
 const storage = getStorage(app);
 
-export { auth, db, storage };
+export { app, db, storage };
